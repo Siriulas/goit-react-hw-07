4e3ad92fa2194637a5463cef9a8ed04d12428dcb
Siriulas <108139838+Siriulas@users.noreply.github.com>
2024-09-24 09:27:16 +0300

    fin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
-
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "../redux/contactsOps";
 import ContactList from "./ContactList";
 import ContactForm from "./ContactForm";
 import SearchBox from "./SearchBox";
 
+export default function App() {
+  const dispatch = useDispatch();
 
-export default function App(){
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
-    return (
-        <div>
-            <SearchBox />
-            <ContactForm />
-            <ContactList />
-        </div>
-
-    )
+  return (
+    <div>
+      <SearchBox />
+      <ContactForm />
+      <ContactList />
+    </div>
+  );
 }
